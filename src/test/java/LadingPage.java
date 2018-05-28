0cf549328307c42e72c959afd686664c956083c4
Veresmag@gmail.com <5551570>
2018-05-29 00:55:55 +0300

    Everad Version 2

--- src/test/java/LadingPage.java
+++ src/test/java/LadingPage.java
@@ -1,6 +1,7 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
@@ -8,35 +9,41 @@ import static java.lang.Thread.sleep;
 
 public class LadingPage {
     private WebDriver webDriver;
-    private WebElement inputNameForm;
-    private WebElement inputPhoneForm;
-    private WebElement signInButton;
+
+    @FindBy (xpath ="//input [@class='block-name']" )
+    private List<WebElement> inputNameForm;
+
+    @FindBy (xpath = "//input [@class='block-phone']")
+    private List<WebElement> inputPhoneForm;
+
+    @FindBy (xpath = "//button [@type='submit']")
+    private List<WebElement> signInButton;
+
+    @FindBy(xpath ="//input [@class='check__field']" )
     private WebElement codeVerification;
+
+    @FindBy (xpath ="//a [@target='_blank']")
     private WebElement privacyPolicy;
+
+    @FindBy(xpath ="//div [@class='checkbtn check__btn hover-shadow']")
     private WebElement buttonCodeVerification;
+
+    @FindBy(xpath ="//span [@class='check__result']")
     private WebElement errorMassageCodeVerification;
+
+    @FindBy(xpath ="//div [@class='clone']")
     private List<WebElement> formList;
 
     public LadingPage(WebDriver webDriver) {
         this.webDriver = webDriver;
-        initElements();
+        PageFactory.initElements(webDriver, this);
     }
 
-    public void initElements() {
-        inputNameForm = webDriver.findElement(By.xpath("//input [@class='block-name']"));
-        inputPhoneForm = webDriver.findElement(By.xpath("//input [@class='block-phone']"));
-        signInButton = webDriver.findElement(By.xpath("//button [@type='submit']"));
-        codeVerification = webDriver.findElement(By.xpath("//input [@class='check__field']"));
-        privacyPolicy = webDriver.findElement(By.xpath("//a [@target='_blank']"));
-        buttonCodeVerification = webDriver.findElement(By.xpath("//div [@class='checkbtn check__btn hover-shadow']"));
-        errorMassageCodeVerification = webDriver.findElement(By.xpath("//span [@class='check__result']"));
-        formList = webDriver.findElements(By.xpath("//div [@class='clone']"));
-    }
 
     public void form(String name, String phone) {
-        inputNameForm.sendKeys(name);
-        inputPhoneForm.sendKeys(phone);
-        signInButton.click();
+        inputNameForm.get(0).sendKeys(name);
+        inputPhoneForm.get(0).sendKeys(phone);
+        signInButton.get(0).click();
     }
 
     public void setCodeVerification(String code) {
@@ -61,13 +68,42 @@ public class LadingPage {
             String formResultText = formResult.getText();
             System.out.println(formResultText);
             System.out.println(formList.size());}
-            inputNameForm.sendKeys(name);
-            inputPhoneForm.sendKeys(phone);
+//            if (formList.contains("Ваши данные не будут переданы третьим лицам!"));
+//                else {}
+            inputNameForm.get(0).sendKeys(name);
+            inputPhoneForm.get(0).sendKeys(phone);
             sleep (5000);
-            signInButton.click();
+            signInButton.get(0).click();
             sleep (5000);
             webDriver.navigate().back();
             sleep (5000);
+
+// Нашел как обратиться к каждой форме, но код получается огромный.
+// Уверен что должен быть цикличный метод.
+
+        inputNameForm.get(1).sendKeys(name);
+        inputPhoneForm.get(1).sendKeys(phone);
+        sleep (5000);
+        signInButton.get(1).click();
+        sleep (5000);
+        webDriver.navigate().back();
+        sleep (5000);
+
+        inputNameForm.get(2).sendKeys(name);
+        inputPhoneForm.get(2).sendKeys(phone);
+        sleep (5000);
+        signInButton.get(2).click();
+        sleep (5000);
+        webDriver.navigate().back();
+        sleep (5000);
+
+        inputNameForm.get(3).sendKeys(name);
+        inputPhoneForm.get(3).sendKeys(phone);
+        sleep (5000);
+        signInButton.get(3).click();
+        sleep (5000);
+        webDriver.navigate().back();
+        sleep (5000);
         }
 }
 
